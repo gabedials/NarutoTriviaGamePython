8295fc1250746923c2ab663551b632faaef595af
gabedials <54412663+gabedials@users.noreply.github.com>
2019-09-16 15:59:50 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#Gabe Dials
+#NarutoTriviaGame
+print("Hello, welcome to Naruto Trivia Game")
+ninjaName = input("What is your name? ")
+print("Your ninja name is", ninjaName)
+print("QUESTION 1")
+print("How many tailed beast are there, nine or ten?")
+print(9+1)
